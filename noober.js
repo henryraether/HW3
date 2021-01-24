60560a5ce44c02d387ae09d8d906f2360a9cb690
Henry Raether <henry.raether@gmail.com>
2021-01-23 18:04:58 -0600

    HW3 V1

--- noober.js
+++ noober.js
@@ -1,12 +1,70 @@
+
 function pageLoad() {
   // 👇 YOUR CODE BEGINS HERE. DON'T CHANGE ANY OTHER CODE. 👇
+  console.log(ride)
+   levelOfService = 'Noober X'
+    
+    
+  let numberOfPassengers = ride.length 
+  console.log(numberOfPassengers)
+  let passenger1Name = ride[0].passengerDetails.first + ' ' + ride[0].passengerDetails.last
+  console.log(passenger1Name)
+  let passenger1phone = ride[0].passengerDetails.phoneNumber
+  console.log(passenger1phone)
+  let passenger1NumberOfPassengers = ride[0].numberOfPassengers
+  console.log(passenger1NumberOfPassengers)
+  let passenger1PickupAddressLine1 = ride[0].pickupLocation.address
+  console.log(passenger1PickupAddressLine1)
+  let passenger1PickupAddressLine2 = ride[0].pickupLocation.city + ', ' + ride[0].pickupLocation.state + ' ' + ride[0].pickupLocation.zip
+  console.log(passenger1PickupAddressLine2)
+  let passenger1DropoffAddressLine1 = ride[0].dropoffLocation.address 
+  console.log(passenger1DropoffAddressLine1) 
+  let passenger1DropoffAddressLine2 = ride[0].dropoffLocation.city + ', ' + ride[0].dropoffLocation.state + ' ' + ride[0].dropoffLocation.zip
+  console.log(passenger1DropoffAddressLine2)
+  let passenger1purpleRequest = ride[0].purpleRequested
+  console.log(passenger1purpleRequest)
 
+  let passenger2Name = ride[1].passengerDetails.first + ' ' + ride[1].passengerDetails.last
+  console.log(passenger2Name)
+  let passenger2Phone = ride[1].passengerDetails.phoneNumber
+  console.log(passenger2Phone)
+  let passenger2NumberOfPassengers = ride[1].numberOfPassengers
+  console.log(passenger2NumberOfPassengers)
+  let passenger2PickupAddressLine1 = ride[1].pickupLocation.address 
+  console.log(passenger2PickupAddressLine1)
+  let passenger2PickupAddressLine2 = ride[1].pickupLocation.city + ', ' + ride[1].pickupLocation.state + ' ' + ride[1].pickupLocation.zip
+  console.log(passenger2PickupAddressLine2)
+  let passenger2DropoffAddressLine1 = ride[1].dropoffLocation.address
+  console.log(passenger2DropoffAddressLine1) 
+  let passenger2DropoffAddressLine2 = ride[1].dropoffLocation.city + ', ' + ride[1].dropoffLocation.state + ' ' + ride[1].dropoffLocation.zip
+  console.log(passenger2DropoffAddressLine2)
+  let passenger2purpleRequest = ride[1].purpleRequested
+  console.log(passenger2purpleRequest)
+
+  let passenger3Name = ride[2].passengerDetails.first + ' ' + ride[2].passengerDetails.last
+  console.log(passenger3Name)
+  let passenger3Phone = ride[2].passengerDetails.phoneNumber
+  console.log(passenger3Phone)
+  let passenger3NumberOfPassengers = ride[2].numberOfPassengers
+  console.log(passenger3NumberOfPassengers)
+  let passenger3PickupAddressLine1 = ride[2].pickupLocation.address
+  console.log(passenger3PickupAddressLine1)
+  let passenger3PickupAddressLine2 = ride[2].pickupLocation.city + ', ' + ride[2].pickupLocation.state + ' ' + ride[2].pickupLocation.zip
+  console.log(passenger3PickupAddressLine2)
+  let passenger3DropoffAddressLine1 = ride[2].dropoffLocation.address
+  console.log(passenger3DropoffAddressLine1) 
+  let passenger3DropoffAddressLine2 = ride[2].dropoffLocation.city + ', ' + ride[2].dropoffLocation.state + ' ' + ride[2].dropoffLocation.zip
+  console.log(passenger3DropoffAddressLine2)
+  let passenger3purpleRequest = ride[2].purpleRequested
+  console.log(passenger3purpleRequest)
+
+  
   // 🔥🔥🔥 YOUR CODE GOES HERE 🔥🔥🔥
   // the 'ride' variable contains data about a random ride (i.e. when the page)
   // is refreshed or reloaded, the 'ride' variable will contain something different
   // to get started, try to write other parts of 'ride' to the JavaScript console in Chrome
   // e.g. ask 'ride' for its length or only grab the first member of the Array
-  console.log(ride)
+
 
   // these variables map to the elements on the finished page;
   // assign the proper value to each variable
