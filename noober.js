f4aa081a32d373882a5475c55cbc7ce17fb31855
Henry Raether <henry.raether@gmail.com>
2021-01-25 17:21:16 -0600

    HW3 V2

--- noober.js
+++ noober.js
@@ -1,70 +1,128 @@
-
 function pageLoad() {
-  // 👇 YOUR CODE BEGINS HERE. DON'T CHANGE ANY OTHER CODE. 👇
-  console.log(ride)
-   levelOfService = 'Noober X'
-    
-    
-  let numberOfPassengers = ride.length 
-  console.log(numberOfPassengers)
-  let passenger1Name = ride[0].passengerDetails.first + ' ' + ride[0].passengerDetails.last
-  console.log(passenger1Name)
-  let passenger1phone = ride[0].passengerDetails.phoneNumber
-  console.log(passenger1phone)
-  let passenger1NumberOfPassengers = ride[0].numberOfPassengers
-  console.log(passenger1NumberOfPassengers)
-  let passenger1PickupAddressLine1 = ride[0].pickupLocation.address
-  console.log(passenger1PickupAddressLine1)
-  let passenger1PickupAddressLine2 = ride[0].pickupLocation.city + ', ' + ride[0].pickupLocation.state + ' ' + ride[0].pickupLocation.zip
-  console.log(passenger1PickupAddressLine2)
-  let passenger1DropoffAddressLine1 = ride[0].dropoffLocation.address 
-  console.log(passenger1DropoffAddressLine1) 
-  let passenger1DropoffAddressLine2 = ride[0].dropoffLocation.city + ', ' + ride[0].dropoffLocation.state + ' ' + ride[0].dropoffLocation.zip
-  console.log(passenger1DropoffAddressLine2)
-  let passenger1purpleRequest = ride[0].purpleRequested
-  console.log(passenger1purpleRequest)
+  // :point_down: YOUR CODE BEGINS HERE. DON'T CHANGE ANY OTHER CODE. :point_down:
 
-  let passenger2Name = ride[1].passengerDetails.first + ' ' + ride[1].passengerDetails.last
-  console.log(passenger2Name)
-  let passenger2Phone = ride[1].passengerDetails.phoneNumber
-  console.log(passenger2Phone)
-  let passenger2NumberOfPassengers = ride[1].numberOfPassengers
-  console.log(passenger2NumberOfPassengers)
-  let passenger2PickupAddressLine1 = ride[1].pickupLocation.address 
-  console.log(passenger2PickupAddressLine1)
-  let passenger2PickupAddressLine2 = ride[1].pickupLocation.city + ', ' + ride[1].pickupLocation.state + ' ' + ride[1].pickupLocation.zip
-  console.log(passenger2PickupAddressLine2)
-  let passenger2DropoffAddressLine1 = ride[1].dropoffLocation.address
-  console.log(passenger2DropoffAddressLine1) 
-  let passenger2DropoffAddressLine2 = ride[1].dropoffLocation.city + ', ' + ride[1].dropoffLocation.state + ' ' + ride[1].dropoffLocation.zip
-  console.log(passenger2DropoffAddressLine2)
-  let passenger2purpleRequest = ride[1].purpleRequested
-  console.log(passenger2purpleRequest)
-
-  let passenger3Name = ride[2].passengerDetails.first + ' ' + ride[2].passengerDetails.last
-  console.log(passenger3Name)
-  let passenger3Phone = ride[2].passengerDetails.phoneNumber
-  console.log(passenger3Phone)
-  let passenger3NumberOfPassengers = ride[2].numberOfPassengers
-  console.log(passenger3NumberOfPassengers)
-  let passenger3PickupAddressLine1 = ride[2].pickupLocation.address
-  console.log(passenger3PickupAddressLine1)
-  let passenger3PickupAddressLine2 = ride[2].pickupLocation.city + ', ' + ride[2].pickupLocation.state + ' ' + ride[2].pickupLocation.zip
-  console.log(passenger3PickupAddressLine2)
-  let passenger3DropoffAddressLine1 = ride[2].dropoffLocation.address
-  console.log(passenger3DropoffAddressLine1) 
-  let passenger3DropoffAddressLine2 = ride[2].dropoffLocation.city + ', ' + ride[2].dropoffLocation.state + ' ' + ride[2].dropoffLocation.zip
-  console.log(passenger3DropoffAddressLine2)
-  let passenger3purpleRequest = ride[2].purpleRequested
-  console.log(passenger3purpleRequest)
-
-  
-  // 🔥🔥🔥 YOUR CODE GOES HERE 🔥🔥🔥
+  // :fire::fire::fire: YOUR CODE GOES HERE :fire::fire::fire:
   // the 'ride' variable contains data about a random ride (i.e. when the page)
   // is refreshed or reloaded, the 'ride' variable will contain something different
   // to get started, try to write other parts of 'ride' to the JavaScript console in Chrome
   // e.g. ask 'ride' for its length or only grab the first member of the Array
+  console.log(ride)
 
+  let passenger1 = ride[0]
+  let passenger2 = ride[1]
+  let passenger3 = ride[2]
+
+  levelOfService = ('Noober X', 'Noober Purple', 'Noober XL', 'Noober Pool')
+  
+  if (ride.length > 1) {
+    levelOfService = 'Noober Pool'
+  } else if (ride[0].purpleRequested == true) {
+    levelOfService = 'Noober Purple'
+  } else if (ride[0].numberOfPassengers > 3) {
+    levelOfService = 'Noober XL'
+  } else {levelOfService = 'Noober X'}
+
+  console.log(levelOfService)
+
+  if (ride.length ==3) {
+  passenger1Name = passenger1.passengerDetails.first + ' ' + passenger1.passengerDetails.last
+  passenger1Phone = passenger1.passengerDetails.phoneNumber
+  passenger1NumberOfPassengers = passenger1.numberOfPassengers
+  passenger1PickupAddressLine1 = passenger1.pickupLocation.address
+  passenger1PickupAddressLine2 = passenger1.pickupLocation.city + ', ' + passenger1.pickupLocation.state + ' ' + passenger1.pickupLocation.zip
+  passenger1DropoffAddressLine1 = passenger1.dropoffLocation.address
+  passenger1DropoffAddressLine2 = passenger1.dropoffLocation.city + ', ' + passenger1.dropoffLocation.state + ' ' + passenger1.dropoffLocation.zip
+
+  passenger2Name = passenger2.passengerDetails.first + ' ' + passenger2.passengerDetails.last
+  passenger2Phone = passenger2.passengerDetails.phoneNumber
+  passenger2NumberOfPassengers = passenger2.numberOfPassengers
+  passenger2PickupAddressLine1 = passenger2.pickupLocation.address
+  passenger2PickupAddressLine2 = passenger2.pickupLocation.city + ', ' + passenger2.pickupLocation.state + ' ' + passenger2.pickupLocation.zip
+  passenger2DropoffAddressLine1 = passenger2.dropoffLocation.address
+  passenger2DropoffAddressLine2 = passenger2.dropoffLocation.city + ', ' + passenger2.dropoffLocation.state + ' ' + passenger2.dropoffLocation.zip
+
+  passenger3Name = passenger3.passengerDetails.first + ' ' + passenger3.passengerDetails.last
+  passenger3Phone = passenger3.passengerDetails.phoneNumber
+  passenger3NumberOfPassengers = passenger3.numberOfPassengers
+  passenger3PickupAddressLine1 = passenger3.pickupLocation.address
+  passenger3PickupAddressLine2 = passenger3.pickupLocation.city + ', ' + passenger3.pickupLocation.state + ' ' + passenger3.pickupLocation.zip
+  passenger3DropoffAddressLine1 = passenger3.dropoffLocation.address
+  passenger3DropoffAddressLine2 = passenger3.dropoffLocation.city + ', ' + passenger3.dropoffLocation.state + ' ' + passenger3.dropoffLocation.zip
+  console.log(passenger1Name)
+  console.log(passenger1Phone)
+  console.log(passenger1NumberOfPassengers)
+  console.log(passenger1PickupAddressLine1)
+  console.log(passenger1PickupAddressLine2)
+  console.log(passenger1DropoffAddressLine1)
+  console.log(passenger1DropoffAddressLine2)
+
+  console.log(passenger2Name)
+  console.log(passenger2Phone)
+  console.log(passenger2NumberOfPassengers)
+  console.log(passenger2PickupAddressLine1)
+  console.log(passenger2PickupAddressLine2)
+  console.log(passenger2DropoffAddressLine1)
+  console.log(passenger2DropoffAddressLine2)
+
+  console.log(passenger3Name)
+  console.log(passenger3Phone)
+  console.log(passenger3NumberOfPassengers)
+  console.log(passenger3PickupAddressLine1)
+  console.log(passenger3PickupAddressLine2)
+  console.log(passenger3DropoffAddressLine1)
+  console.log(passenger3DropoffAddressLine2)
+  
+  } else if (ride.length == 2) {
+  passenger1Name = passenger1.passengerDetails.first + ' ' + passenger1.passengerDetails.last
+  passenger1Phone = passenger1.passengerDetails.phoneNumber
+  passenger1NumberOfPassengers = passenger1.numberOfPassengers
+  passenger1PickupAddressLine1 = passenger1.pickupLocation.address
+  passenger1PickupAddressLine2 = passenger1.pickupLocation.city + ', ' + passenger1.pickupLocation.state + ' ' + passenger1.pickupLocation.zip
+  passenger1DropoffAddressLine1 = passenger1.dropoffLocation.address
+  passenger1DropoffAddressLine2 = passenger1.dropoffLocation.city + ', ' + passenger1.dropoffLocation.state + ' ' + passenger1.dropoffLocation.zip
+
+  passenger2Name = passenger2.passengerDetails.first + ' ' + passenger2.passengerDetails.last
+  passenger2Phone = passenger2.passengerDetails.phoneNumber
+  passenger2NumberOfPassengers = passenger2.numberOfPassengers
+  passenger2PickupAddressLine1 = passenger2.pickupLocation.address
+  passenger2PickupAddressLine2 = passenger2.pickupLocation.city + ', ' + passenger2.pickupLocation.state + ' ' + passenger2.pickupLocation.zip
+  passenger2DropoffAddressLine1 = passenger2.dropoffLocation.address
+  passenger2DropoffAddressLine2 = passenger2.dropoffLocation.city + ', ' + passenger2.dropoffLocation.state + ' ' + passenger2.dropoffLocation.zip
+
+  console.log(passenger1Name)
+  console.log(passenger1Phone)
+  console.log(passenger1NumberOfPassengers)
+  console.log(passenger1PickupAddressLine1)
+  console.log(passenger1PickupAddressLine2)
+  console.log(passenger1DropoffAddressLine1)
+  console.log(passenger1DropoffAddressLine2)
+
+  console.log(passenger2Name)
+  console.log(passenger2Phone)
+  console.log(passenger2NumberOfPassengers)
+  console.log(passenger2PickupAddressLine1)
+  console.log(passenger2PickupAddressLine2)
+  console.log(passenger2DropoffAddressLine1)
+  console.log(passenger2DropoffAddressLine2)
+  
+  } else {
+  passenger1Name = passenger1.passengerDetails.first + ' ' + passenger1.passengerDetails.last
+  passenger1Phone = passenger1.passengerDetails.phoneNumber
+  passenger1NumberOfPassengers = passenger1.numberOfPassengers
+  passenger1PickupAddressLine1 = passenger1.pickupLocation.address
+  passenger1PickupAddressLine2 = passenger1.pickupLocation.city + ', ' + passenger1.pickupLocation.state + ' ' + passenger1.pickupLocation.zip
+  passenger1DropoffAddressLine1 = passenger1.dropoffLocation.address
+  passenger1DropoffAddressLine2 = passenger1.dropoffLocation.city + ', ' + passenger1.dropoffLocation.state + ' ' + passenger1.dropoffLocation.zip
+
+  console.log(passenger1Name)
+  console.log(passenger1Phone)
+  console.log(passenger1NumberOfPassengers)
+  console.log(passenger1PickupAddressLine1)
+  console.log(passenger1PickupAddressLine2)
+  console.log(passenger1DropoffAddressLine1)
+  console.log(passenger1DropoffAddressLine2)
+  }
+  
 
   // these variables map to the elements on the finished page;
   // assign the proper value to each variable
@@ -80,5 +138,5 @@ function pageLoad() {
   // passenger3PickupAddressLine1, passenger3PickupAddressLine2
   // passenger3DropoffAddressLine1, passenger3DropoffAddressLine2
   
-  //  👆 YOUR CODE ENDS HERE. DON'T CHANGE ANY OTHER CODE 👆
+  //  :point_up_2: YOUR CODE ENDS HERE. DON'T CHANGE ANY OTHER CODE :point_up_2:
 }
